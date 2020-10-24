c11612e036acb29ffa6aef47a8235139c4e43a48
Timofey Zhdanovich <zhdanovich.tv@phystech.edu>
2020-10-24 08:49:43 +0000

    n m max sum version 1.0

--- D-wave problem n m.py
+++ D-wave problem n m.py
@@ -9,9 +9,8 @@ a = [0 for i in range(n)]
 b = [0 for j in range(n)]
 x = pyqubo.Array.create('x', shape=n, vartype='BINARY')
 a = list(map(int, input().split()))
-for i in range(n):
-    b[i] = -4*m*max(a) - a[i]
-H = sum(w * s for s, w in zip(x, b)) + 2 * max(a) * sum(s for s in x)**2
+b = list(map(abs, a))
+H = 100 * max(b) * (sum(b for b in x) - m)**2  - max (b) * sum(k * b for k, b in zip(a, x))
 Q, offset = H.compile().to_qubo()
 sampleset = sampler.sample_qubo(Q, num_reads=5000)
 print(sampleset)
