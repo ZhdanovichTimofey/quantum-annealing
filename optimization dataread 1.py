e745f751953629ab66538143f6518d908ae6fe66
ZhdanovichTimofey <71005308+ZhdanovichTimofey@users.noreply.github.com>
2020-11-16 20:53:46 +0300

    версия 2, измененена оценка эффективности, распределение бюджета

--- optimization dataread 1.py
+++ optimization dataread 1.py
@@ -4,19 +4,16 @@ from dwave.system import DWaveSampler, EmbeddingComposite
 import dimod
 import pyqubo
 import neal
-names = ['KRKNP', 'MFGS', 'GAZP', 'SIBN', 'BANEP', 'RNFT', 'ROSN', 'LKOH',
-         'TATN', 'USBN', 'BSPB', 'VTBR', 'SBER', 'KUZB', 'PRMB', 'ROSB', 'CBOM',
-         'TCSG', 'KOGK', 'UKUZ', 'ALNU', 'RASP']
-neftegaz = [['KRKNP', 2.84, 11700], ['MFGS', 3.15, 284], ['GAZP', 3.25, 165.27],
-            ['SIBN', 3.41, 288.1], ['BANEP', 3.48, 1567], ['RNFT', 3.61, 299.2],
-            ['ROSN', 4.42, 382.8], ['LKOH', 4.50, 4451.5],
-            ['TATN', 5.62, 453.7], ['MEAN', 7.57, 0]]
-banks = [['USBN', 1.53, 0.0656], ['BSPB', 2.70, 44.9], ['VTBR', 4.76, 0.034005],
-         ['SBER', 5.89, 220.8], ['KUZB', 6.62, 0.0143], ['PRMB', 7.53, 25400],
-         ['ROSB', 11.1, 78], ['CBOM', 11.3, 5.665], ['TCSG', 11.9, 2184.4],
-         ['MEAN', 13.8, 0]]
-gornodob = [['KOGK', 1.90, 45000], ['UKUZ', 2.67, 496], ['ALNU', 2.72, 56800],
-            ['RASP', 5.98, 116.06], ['MEAN', 7.30, 0]]
+names = ['KRKNP', 'MFGS', 'USBN', 'BSPB', 'KOGK', 'UKUZ',
+         'AMEZ', 'URKZ', 'ACKO', 'SFIN', 'OBUV', 'FEES', 'MRKV']
+neftegaz = [['KRKNP', 2.84, 11700], ['MFGS', 3.15, 284], ['GAZP', 3.25, 165.27]]
+banks = [['USBN', 1.53, 0.0656], ['BSPB', 2.70, 44.9], ['VTBR', 4.76, 0.034005]]
+gornodob = [['KOGK', 1.90, 45000], ['UKUZ', 2.67, 496], ['ALNU', 2.72, 56800]]
+metal = [['AMEZ', 1.52, 4.64], ['URKZ', 1.62, 14920], ['CHMK', 2.04, 2525]]
+finance = [['ACKO', 2.77, 4.66], ['SFIN', 5.68, 468.2], ['AFKS', 5.72, 31.649]]
+retail = [['OBUV', 2.12, 31.65], ['MEAN', 5.71, 0], ['MEAN', 5.71, 0]]
+elseti = [['FEES', 2.92, 0.1984], ['MRKV', 3.93, 0.06975],
+          ['MRKP', 3.92, 0.2327]]
 B = 1000000
 sampler = EmbeddingComposite(DWaveSampler())
 Ef = []
@@ -26,12 +23,20 @@ matrix_covariation = [[] for k in range(len(names))]
 
 
 def efficiency(E):
-    for i in range(len(neftegaz) - 1):
-        E += [neftegaz[i][2] * (neftegaz[i + 1][1] - neftegaz[i][1])]
-    for i in range(len(banks) - 1):
-        E += [banks[i][2] * (banks[i + 1][1] - banks[i][1])]
-    for i in range(len(gornodob) - 1):
-        E += [gornodob[i][2] * (gornodob[i + 1][1] - gornodob[i][1])]
+    for i in range(2):
+        E += [neftegaz[2][1] - neftegaz[i][1]]
+    for i in range(2):
+        E += [banks[2][1] - banks[i][1]]
+    for i in range(2):
+        E += [gornodob[2][1] - gornodob[i][1]]
+    for i in range(2):
+        E += [metal[2][1] - metal[i][1]]
+    for i in range(2):
+        E += [finance[2][1] - finance[i][1]]
+    for i in range(2):
+        E += [retail[2][1] - retail[i][1]] \
+    for i in range(2):
+        E += [elseti[2][1] - elseti[i][1]]
     return E
 
 
@@ -61,18 +66,19 @@ def create_matrix_covariation(matrix_covariation):
     return matrix_covariation
 
 
-Ef = efficiency(Ef)
+B1 = B / len(names)
+Ef = B1 * efficiency(Ef)
 f = data_read(f)
 matrix_covariation = create_matrix_covariation(matrix_covariation)
-A = []
+'''A = []
 for i in range(len(Ef)):
-    A += [B*2 / len(Ef)]
+    A += [B*2 / len(Ef)]'''
 x = pyqubo.Array.create('x', shape=len(Ef), vartype='BINARY')
 H1 = -sum(a * e for a, e in zip(x, Ef))
 for i in range(len(Ef)):
     for j in range(len(Ef)):
         H1 += x[i] * x[j] * matrix_covariation[i][j]
-H2 = (sum(a * A for a, A in zip(x, A)) - B)**2
+#H2 = (sum(a * A for a, A in zip(x, A)) - B)**2
 H = H1 + H2
 Q, offset = H.compile().to_qubo()
 sampleset = sampler.sample_qubo(Q, num_reads=5000)
